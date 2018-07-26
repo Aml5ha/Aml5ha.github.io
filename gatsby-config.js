b337151f57c54bce3208a870c6ffbaa453d1683a
Arman Lokhandwala <aml5ha@virginia.edu>
2018-07-26 00:00:52 -0400

    Fixed cite naming issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Ceevee",
-    author: "Aman Mittal",
-    description: "A Gatsby.js Starter based on Ceevee by Styleshout"
+    title: "Welcome to My World",
+    author: "Arman Lokhandwala",
+    description: "A website introducing Arman Lokhandwala"
   },
   pathPrefix: "/",
   plugins: [
