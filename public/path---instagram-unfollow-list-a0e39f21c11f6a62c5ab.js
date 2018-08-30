e86fd688ee93dc10b430ac2b43322524a4873d35
Arman Lokhandwala <aml5ha@virginia.edu>
2018-08-29 20:31:24 -0400

    updated typo

---
+++ public/path---instagram-unfollow-list-a0e39f21c11f6a62c5ab.js
@@ -0,0 +1,2 @@
+webpackJsonp([87101832311538],{419:function(t,n){t.exports={pathContext:{}}}});
+//# sourceMappingURL=path---instagram-unfollow-list-a0e39f21c11f6a62c5ab.js.map
--- src/components/About.js
+++ src/components/About.js
@@ -14,11 +14,11 @@ const About = () => (
       <div className="nine columns main-col">
         <h2>About Me</h2>
         <p>
-          As mentioned before, I am <span>Arman Lokhandwala</span>, and I currently studying <span>Computer Science</span> at the University of Virginia in Charlottesville, VA. 
-          I am a determined individual who enjoys working on teams and am interested in software development and design. I am firm believer that the advancement of technology will help better the quality of life of those around the world.  
+          As mentioned before, I am <span>Arman Lokhandwala</span>, and I'm currently studying <span>Computer Science</span> at the University of Virginia in Charlottesville, VA. 
+          I am a determined individual who enjoys working on teams and am interested in software development and design. I am a firm believer that the advancement of technology will help better the quality of life of those around the world.  
           <br/> <br/>
           
-          My passions include technology, cars, games, science and astronomy, psychology, teaching, and most importantly helping people by empowering them. 
+          My passions include technology, cars, games, science and astronomy, psychology, teaching, and most importantly, helping people by empowering them. 
           In my free time I like to research cars to learn how new technologies are being implemented in them. I also enjoy playing and learning about new games, video games and board games alike! 
           As far as science, astronomy, and psychology go, I try to stay up to date by reading articles and watching videos/documentaries. 
           Lastly, I strive to help people everyday by being a role model and offering to help teach them anything I have to offer - whether it be academic or life skills. 
